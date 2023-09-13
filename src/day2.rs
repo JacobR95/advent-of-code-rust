015619c6099280715c990c67f28b43fc2f46ce56
JacobR95 <jrobinson@apriori.com>
2023-09-12 21:16:49 -0700

    Start day 2

---
+++ src/day2.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+pub fn day2() -> u32 {
+    // TODO
+}
+
+#[cfg(test)]
+mod day2_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_expected_amount() {
+        let result = day2();
+        assert_eq!(result, 200);
+    }
+}

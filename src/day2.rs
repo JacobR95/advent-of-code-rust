7afe0a592ccc69712f921ec6ba67f9d08646a6f1
JacobR95 <jrobinson@apriori.com>
2023-09-17 15:44:07 -0700

    Start day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -96,12 +96,51 @@ pub fn day2() -> u32 {
     return total_score;
 }
 
+// --- Part Two ---
+// The Elf finishes helping with the tent and sneaks back over to you.
+// "Anyway, the second column says how the round needs to end:
+// X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+//
+// The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated.
+// The example above now goes like this:
+//
+// In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock.
+// This gives you a score of 1 + 3 = 4.
+// In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+// In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+// Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+//
+// Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+pub fn day2_part2() -> u32 {
+
+    let data = fs::read_to_string("data/day2.txt").unwrap();
+
+    let mut total_score = 0;
+
+    data.lines().for_each(|line| {
+
+        if line.is_empty() {
+            return;
+        }
+
+        //  TODO
+    });
+
+    return total_score;
+}
+
 #[cfg(test)]
 mod day2_tests {
     use super::*;
 
     #[test]
     fn should_return_expected_amount() {
+        let result = day2();
+        assert_eq!(result, 11841);
+    }
+
+    #[test]
+    fn should_return_expected_amount_for_part_2() {
         let result = day2();
         assert_eq!(result, 11841);
     }

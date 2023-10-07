e410a9697cdca065ffec199bdac213366571fe86
Jacob Robinson <jrobinson@apriori.com>
2023-10-07 15:11:10 -0700

    Start day3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     println!("day1: {}", day1::day1());
     println!("day1_part2: {}", day1::day1_part2());
     println!("day2: {}", day2::day2());
+    println!("day2_part2: {}", day2::day2_part2());
+    println!("day3: {}", day3::day3());
+    println!("day3_part2: {}", day3::day3_part2());
 }

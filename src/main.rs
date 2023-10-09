fdfbf3e3e815f910dceb2148dc2ef42d652bf9f4
JacobR95 <jrobinson@apriori.com>
2023-10-09 15:45:52 -0700

    Complete day4 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("day1: {}", day1::day1());
@@ -9,4 +10,6 @@ fn main() {
     println!("day2_part2: {}", day2::day2_part2());
     println!("day3: {}", day3::day3());
     println!("day3_part2: {}", day3::day3_part2());
+    println!("day4: {}", day4::day4());
+    println!("day4_part2: {}", day4::day4_part2());
 }

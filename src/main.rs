30fd483e9b3c4b50668807bcab6083878da806dc
JacobR95 <jrobinson@apriori.com>
2023-11-12 14:44:19 -0800

    Fix linting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use crate::day5::DAY5_TEST_DATA;
-use std::fs;
 
 mod day1;
 mod day2;
@@ -16,6 +15,6 @@ fn main() {
     println!("day3_part2: {}", day3::day3_part2());
     println!("day4: {}", day4::day4());
     println!("day4_part2: {}", day4::day4_part2());
-    println!("day5: {}", day5::day5(String::from(fs::read_to_string("data/day5.txt").unwrap())));
-    println!("daty5: {}", day5::day5_part2(String::from(fs::read_to_string("data/day5.txt").unwrap())));
-}
+    println!("day5: {}", day5::day5(DAY5_TEST_DATA.to_string()));
+    println!("daty5: {}", day5::day5_part2(DAY5_TEST_DATA.to_string()));
+ }
